5fcac39bbb6e1b3e93b9802ef44d0f0ef8ea0280
Patel Dharmesh <pateldharmesh8262@gmail.com>
2024-04-13 15:39:15 +0530

    Feat: Added status feature to enhance user experience

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,50 @@
 import { Link } from "react-router-dom";
+import useStatus from "../utils/useStatus";
 
 const Title = () => {
-    return (
-      <Link to ="/">
-        <img
-          className="logo"
-          alt="logo"
-          src="https://files.yappe.in/place/full/food-villa-family-restaurant-hotel-4642683.webp"
-        />
-      </Link>
-    );
-  };
-  
-const Header = () => {
-    return (
-      <div className="header">
-        <Title />
-        <div className="nav-items">
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li>Cart</li>
-          </ul>
-        </div>
-      </div>
-    );
-  };
+  return (
+    <Link to="/">
+      <img
+        className="logo"
+        alt="logo"
+        src="https://files.yappe.in/place/full/food-villa-family-restaurant-hotel-4642683.webp"
+      />
+    </Link>
+  );
+};
 
-  export default Header;
+
+const Header = () => {
+
+  const isOnline = useStatus();
+
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <div className="status-tracker">
+          Status:
+          {isOnline ? (
+            <div className="online-status"></div>
+          ) : (
+            <div className="offline-status"></div>
+          )}
+        </div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
