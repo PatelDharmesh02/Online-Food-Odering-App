64144cfd5196f2553b2df81b32d7c6e608093436
Patel Dharmesh <pateldharmesh8262@gmail.com>
2024-04-13 15:41:27 +0530

    Smaple: Sample Class Component

---
+++ src/components/ProfileClass.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>This is the Class based Profile Component</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >Click</button>
+      </div>
+    );
+  }
+}
+
+export default Profile;
