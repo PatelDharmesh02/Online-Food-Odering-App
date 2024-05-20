89d2243e930188f5352d1a3c4d809b119450e693
Patel Dharmesh <pateldharmesh8262@gmail.com>
2024-05-21 00:47:23 +0530

    feat: Added feature to store the restaurants details to avoid network request on every render

--- src/utils/useRestuarantDetails.js
+++ src/utils/useRestuarantDetails.js
@@ -1,11 +1,22 @@
 import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateRestaurantsDetails } from "../redux/restaurantsSlice";
 
 const useRestuarantDetails = (id) => {
   const [restuarantInfo, setRestaurantInfo] = useState(null);
   const [restaurantMenu, setRestaurantMenu] = useState(null);
+  const restaurantsDetails = useSelector(
+    (state) => state.restaurants.restaurantsDetails
+  );
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    getRestaurantDetails();
+    if (restaurantsDetails.hasOwnProperty(id)) {
+      setRestaurantInfo(restaurantsDetails[id].info);
+      setRestaurantMenu(restaurantsDetails[id].menu);
+    } else {
+      getRestaurantDetails();
+    }
   }, []);
 
   const getRestaurantDetails = async () => {
@@ -15,10 +26,17 @@ const useRestuarantDetails = (id) => {
         "&catalog_qa=undefined&isMenuUx4=true&submitAction=ENTER"
     );
     const json = await data?.json();
-    setRestaurantInfo(json?.data?.cards?.[2]?.card?.card?.info);
-    setRestaurantMenu(
-      json?.data?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards
-    );
+    const restroInfo = json?.data?.cards?.[2]?.card?.card?.info;
+    const restroMenu =
+      json?.data?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+    setRestaurantInfo(restroInfo);
+    setRestaurantMenu(restroMenu);
+    let payload = {};
+    payload[id] = {
+      info: restroInfo,
+      menu: restroMenu,
+    };
+    dispatch(updateRestaurantsDetails({ ...restaurantsDetails, ...payload }));
   };
 
   return [restuarantInfo, restaurantMenu];
--- src/utils/useRestuarantsData.js
+++ src/utils/useRestuarantsData.js
@@ -1,11 +1,19 @@
 import { useState, useEffect } from "react";
 import { RESTUARANT_DATA_URL } from "../constants";
+import { useDispatch, useSelector } from "react-redux";
+import { updateRestaurantsData } from "../redux/restaurantsSlice";
 
 const useRestuarantsData = () => {
   const [allRestuarants, setAllRestuarants] = useState([]);
+  const restaurantsData = useSelector(state => state.restaurants.restaurantsData);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    getRestaurants();
+    if (restaurantsData && restaurantsData.length > 0) {
+      setAllRestuarants(restaurantsData);
+    } else {
+      getRestaurants();
+    }
   }, []);
 
   async function getRestaurants() {
@@ -15,6 +23,7 @@ const useRestuarantsData = () => {
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants;
     setAllRestuarants(restros);
+    dispatch(updateRestaurantsData(restros));
   }
 
   return allRestuarants;
