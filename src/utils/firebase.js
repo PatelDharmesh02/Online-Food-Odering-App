727e5452bc4b5955c93353a152276df84e63116f
Patel Dharmesh <pateldharmesh8262@gmail.com>
2024-09-13 09:24:44 +0530

    Feat: Set up Firebase for authentication

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDow3-02Fknt5kThMNxSCNNdUparT39Q6g",
+  authDomain: "platter-food-delivery-app.firebaseapp.com",
+  projectId: "platter-food-delivery-app",
+  storageBucket: "platter-food-delivery-app.appspot.com",
+  messagingSenderId: "1033840907428",
+  appId: "1:1033840907428:web:3cffd6912e9da3b1bb4231",
+  measurementId: "G-DZBK0XDNFG",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
