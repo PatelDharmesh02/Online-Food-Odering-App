f270002d4d4c4b69385cf647766981d1910d4e39
Patel Dharmesh <pateldharmesh8262@gmail.com>
2024-10-05 15:53:56 +0530

    Feat: Added Authentication and user Avatar and Icons for the header buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom";
 import useStatus from "../utils/useStatus";
 import logo from "../assets/Platter.png";
-import AccountLogo from "../assets/AccountIcon.png";
 import { useDispatch, useSelector } from "react-redux";
-import { Drawer, TextField } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-import { useRef, useState } from "react";
+import { Drawer, TextField, Popover, Button, Avatar } from "@mui/material";
+import { Close, Logout } from "@mui/icons-material";
+import { useRef, useState, useCallback } from "react";
 import { ValidateData } from "../utils/validate";
 import { setUser } from "../redux/userSlice";
 import { auth } from "../utils/firebase";
@@ -14,39 +13,59 @@ import {
   signInWithEmailAndPassword,
   updateProfile,
 } from "firebase/auth";
-import Profile from "./Profile";
-
-const Title = () => {
-  return (
-    <Link to="/">
-      <div className="flex items-center p-2 gap-2 justify-center">
-        <img
-          data-testid="logo"
-          className="w-12 h-12 md:w-20 md:h-20"
-          alt="logo"
-          src={logo}
-        />
-        <p className="font-light text-2xl md:text-4xl font-serif">Platter</p>
-      </div>
-    </Link>
-  );
-};
+import {
+  ConnectWithoutContactOutlined,
+  EventOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
 
 const Header = () => {
   const [openDrawer, setOpenDrawer] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
   const [login, setLogin] = useState(true);
-  const isOnline = useStatus();
+  const [error, setError] = useState(null);
+
   const items = useSelector((store) => store.cart.items);
   const userData = useSelector((store) => store.user.userData);
+  const isOnline = useStatus();
+  const dispatch = useDispatch();
+
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
   const userNameRef = useRef(null);
-  const [error, setError] = useState(null);
-  const dispatch = useDispatch();
 
   const toggleDrawer = () => {
     setOpenDrawer(!openDrawer);
   };
+
+  const getProfilePhoto = useCallback(
+    (user) => {
+      const { photoUrl, displayName, email } = user;
+      if (photoUrl) {
+        return { src: photoUrl };
+      }
+      const letters = (displayName || email).toUpperCase().split(" ");
+      return {
+        sx: {
+          bgcolor: stringToColor(displayName),
+        },
+        children: (function () {
+          return letters.map((letter) => letter.charAt(0));
+        })(),
+      };
+    },
+    [userData]
+  );
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
+
   const handleSubmit = () => {
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
@@ -62,7 +81,8 @@ const Header = () => {
           updateProfile(user, {
             displayName: userName,
           })
-            .then((user) => {
+            .then(() => {
+              const user = auth.currentUser;
               dispatch(setUser(user));
               setOpenDrawer(false);
             })
@@ -71,7 +91,6 @@ const Header = () => {
             });
         })
         .catch((error) => {
-          // Handle user creation errors
           const errorCode = error.code;
           const errorMessage = error.message;
           setError(`${errorCode}: ${errorMessage}`);
@@ -90,9 +109,20 @@ const Header = () => {
         });
     }
   };
+
   return (
     <div className="md:flex justify-between pl-3 pr-3 shadow-lg bg-white rounded-lg sticky top-0 right-0 left-0  z-10">
-      <Title />
+      <Link to="/">
+        <div className="flex items-center p-2 gap-2 justify-center">
+          <img
+            data-testid="logo"
+            className="w-12 h-12 md:w-20 md:h-20"
+            alt="logo"
+            src={logo}
+          />
+          <p className="font-light text-2xl md:text-4xl font-serif">Platter</p>
+        </div>
+      </Link>
       <div className="flex justify-between items-center gap-4 pb-2 md:pb-0">
         <div className="text-sm md:text-normal flex gap:1 md:gap-2 items-center bg-yellow-100 p-1 md:p-2 rounded-full">
           Status:
@@ -104,24 +134,54 @@ const Header = () => {
         </div>
         <ul className="flex gap-4">
           <li className="p-1 md:p-2 bg-slate-100 rounded-full">
-            <Link to="/">Home</Link>
+            <Link
+              className="font-medium text-lg flex items-center gap-1"
+              to="/"
+            >
+              <HomeOutlined />
+              <span>Home</span>
+            </Link>
           </li>
           <li className="p-1 md:p-2 bg-slate-100 rounded-full">
-            <Link to="/about">About</Link>
+            <Link
+              className="font-medium text-lg flex items-center gap-1"
+              to="/about"
+            >
+              <EventOutlined />
+              <span>About</span>
+            </Link>
           </li>
           <li className="p-1 md:p-2 bg-slate-100 rounded-full">
-            <Link to="/contact">Contact</Link>
+            <Link
+              className="font-medium text-lg flex items-center gap-1"
+              to="/contact"
+            >
+              <ConnectWithoutContactOutlined /> <span>Contact</span>
+            </Link>
           </li>
           <li className="p-1 md:p-2 bg-slate-100 rounded-full">
-            <Link data-testid="cart" to="/cart">
-              Cart - {items?.length}
+            <Link
+              className="font-medium text-lg flex items-center gap-1"
+              data-testid="cart"
+              to="/cart"
+            >
+              <ShoppingCartOutlined />
+              <span>Cart -</span> {items?.length}
             </Link>
           </li>
         </ul>
       </div>
       <div className="flex items-center justify-center">
         {userData ? (
-          <Profile />
+          <div className="cursor-pointer">
+            <Avatar
+              {...getProfilePhoto(userData)}
+              alt="Profile Picture"
+              onClick={(e) => {
+                setAnchorEl(e.target);
+              }}
+            />
+          </div>
         ) : (
           <span
             className="font-bold text-sm p-1 md:p-2 text-wrap cursor-pointer"
@@ -141,7 +201,7 @@ const Header = () => {
         >
           <div>
             <div className="p-2 m-2 flex flex-col gap-10">
-              <CloseIcon
+              <Close
                 sx={{ cursor: "pointer" }}
                 onClick={() => toggleDrawer()}
               />
@@ -224,6 +284,28 @@ const Header = () => {
             </div>
           </div>
         </Drawer>
+        <Popover
+          id={"profile-popover"}
+          open={Boolean(anchorEl)}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+        >
+          <div className="p-4 min-w-48">
+            <Button
+              variant="contained"
+              color="error"
+              fullWidth
+              startIcon={<Logout />}
+              onClick={handleLogout}
+            >
+              Log Out
+            </Button>
+          </div>
+        </Popover>
       </div>
     </div>
   );
