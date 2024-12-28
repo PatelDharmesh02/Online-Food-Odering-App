ca1e787fd200e9ce6824e9b011744cf1481eb69c
Patel Dharmesh <pateldharmesh8262@gmail.com>
2024-12-28 20:51:05 +0530

    Feat: Added payment integration

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,20 +1,134 @@
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import RestaurantItemCard from "./RestaurantItemCard";
 import EmptyCartLogo from "../assets/EmptyCartIcon.png";
+import { Snackbar, Alert } from "@mui/material";
+import { RAZORPAY_SCRIPT_API } from "../constants";
+import logo from "../assets/Platter.png";
 import { Link } from "react-router-dom";
 import { toggleAuthDrawer } from "../redux/userSlice";
+import { clearCart } from "../redux/cartSlice";
+import axios from "axios";
 
 const Cart = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const items = useSelector((store) => store.cart?.items);
   const totalPrice = useSelector((store) => store.cart?.totalPrice);
   const { userData } = useSelector((store) => store.user);
 
-  const handlePayment = () => {
+  const [showSnackbar, setShowSnackbar] = useState(false);
+  const [alertContent, setAlertContent] = useState({});
+
+  function loadScript(src) {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+      document.body.appendChild(script);
+    });
+  }
+
+  const handlePayment = async () => {
     if (!userData) {
       dispatch(toggleAuthDrawer(true));
       return;
     }
+    const res = await loadScript(RAZORPAY_SCRIPT_API);
+
+    if (!res) {
+      setAlertContent({
+        variant: "filled",
+        severity: "error",
+        children: "Failed to RazorPay Payment Gateway!!",
+      });
+      setShowSnackbar(true);
+      return;
+    }
+
+    const result = await axios.post(process.env.REACT_APP_CREATE_ORDER, {
+      items: items.map((item) => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        quantity: item.count,
+      })),
+      totalPrice,
+      userId: userData.uid,
+    });
+
+    if (!result) {
+      setAlertContent({
+        variant: "filled",
+        severity: "error",
+        children: "Failed to Create Order!!",
+      });
+      setShowSnackbar(true);
+      return;
+    }
+
+    const { amount, id: order_id, currency } = result.data;
+
+    const options = {
+      key: process.env.REACT_APP_RAZORPAY_KEY,
+      amount: amount.toString(),
+      currency: currency,
+      name: "Platter Limited",
+      description: "Test Transaction",
+      image: { logo },
+      order_id: order_id,
+      handler: async function (response) {
+        const data = {
+          orderCreationId: order_id,
+          razorpayPaymentId: response.razorpay_payment_id,
+          razorpayOrderId: response.razorpay_order_id,
+          razorpaySignature: response.razorpay_signature,
+        };
+        const result = await axios.post(
+          process.env.REACT_APP_ORDER_SUCCESS,
+          data
+        );
+        if (result.status === 200) {
+          setAlertContent({
+            variant: "filled",
+            severity: "success",
+            children: "Order Placed Successfully!!",
+          });
+          setShowSnackbar(true);
+          setTimeout(() => {
+            dispatch(clearCart());
+            navigate("/");
+          }, 1500);
+        } else {
+          setAlertContent({
+            variant: "filled",
+            severity: "error",
+            children: "Failed to Place Order!!",
+          });
+          setShowSnackbar(true);
+        }
+      },
+      prefill: {
+        name: userData?.displayName ?? "",
+        email: "Platter.com",
+        contact: "+91-9988776655",
+      },
+      notes: {
+        address: "Platter Corporate Office",
+      },
+      theme: {
+        color: "#FFD580",
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
   };
 
   return items.length > 0 ? (
@@ -71,6 +185,17 @@ const Cart = () => {
           </p>
         </button>
       </div>
+      <Snackbar
+        open={showSnackbar}
+        autoHideDuration={2000}
+        onClose={() => setShowSnackbar(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <Alert {...alertContent} />
+      </Snackbar>
     </>
   ) : (
     <div className="my-40 flex flex-col justify-center items-center gap-3">
